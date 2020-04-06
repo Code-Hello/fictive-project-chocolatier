aebbf06dc9dc425a9b7f7881cfafb4f0d511eeed
Code-Hello <m.tuto@protonmail.com>
2020-04-06 12:05:51 +0200

    navbar changes

--- main.js
+++ main.js
@@ -1,4 +1,20 @@
 /* Navbar */
+/*Menu toggle*/
+const navHeader = document.body.querySelector('#navbar_header');
+const burgerMenu = document.body.querySelector('#menu-burger');
+
+function dropdownMenu() {
+    navHeader.classList.toggle('deployed');
+};
+
+burgerMenu.addEventListener("click", dropdownMenu);
+
+/*Menu burger*/
+const burgerCross = document.querySelector('.menu-toggle');
+
+burgerCross.addEventListener('click', () => {
+    burgerCross.classList.toggle('burgerStyle');
+})
 
 /* Carousel */
 
